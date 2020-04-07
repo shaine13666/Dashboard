a424190bd6df0a05abf1ddd896f91e01a3116824
korehind <korehin.d@roskvartal.ru>
2020-04-07 21:57:12 +0300

    добавил тестовый график

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -8,17 +8,20 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
 import { MatMenuModule } from '@angular/material/menu';
+import { RouterModule } from '@angular/router';
 
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
-import { RouterModule } from '@angular/router';
+import { LineComponent } from './widgets/line/line.component';
+import { HighchartsChartModule } from 'highcharts-angular';
 
 @NgModule({
   declarations: [
     HeaderComponent,
     FooterComponent,
-    SidebarComponent
+    SidebarComponent,
+    LineComponent
   ],
   imports: [
     CommonModule,
@@ -30,12 +33,14 @@ import { RouterModule } from '@angular/router';
     MatSidenavModule,
     MatListModule,
     MatMenuModule,
-    RouterModule
+    RouterModule,
+    HighchartsChartModule
   ],
   exports: [
     HeaderComponent,
     FooterComponent,
-    SidebarComponent
+    SidebarComponent,
+    LineComponent
   ]
 })
 export class SharedModule { }
